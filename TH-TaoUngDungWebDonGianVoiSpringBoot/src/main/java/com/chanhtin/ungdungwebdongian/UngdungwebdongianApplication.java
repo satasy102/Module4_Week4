270dfc848a5adea1e5171e6b2ebcbe2c81602868
Do Chanh Tin <@gmail>
2020-11-18 19:47:24 +0700

    update

---
+++ TH-TaoUngDungWebDonGianVoiSpringBoot/src/main/java/com/chanhtin/ungdungwebdongian/UngdungwebdongianApplication.java
@@ -0,0 +1,13 @@
+package com.chanhtin.ungdungwebdongian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UngdungwebdongianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UngdungwebdongianApplication.class, args);
+    }
+
+}
---
+++ TH-TaoUngDungWebDonGianVoiSpringBoot/src/test/java/com/chanhtin/ungdungwebdongian/UngdungwebdongianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanhtin.ungdungwebdongian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UngdungwebdongianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
